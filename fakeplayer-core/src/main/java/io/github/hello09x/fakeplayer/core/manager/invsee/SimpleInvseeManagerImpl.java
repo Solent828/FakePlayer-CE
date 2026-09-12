d9cbec3f4d9d51943cd58ff49d9e9dc294aa0c7d
Solent828 <romesebastianuk@gmail.com>
2026-09-12 18:09:05 +0100

    Add operator inventory and saved bot integration

--- fakeplayer-core/src/main/java/io/github/hello09x/fakeplayer/core/manager/invsee/SimpleInvseeManagerImpl.java
+++ fakeplayer-core/src/main/java/io/github/hello09x/fakeplayer/core/manager/invsee/SimpleInvseeManagerImpl.java
@@ -6,15 +6,19 @@ import io.github.hello09x.devtools.core.utils.ComponentUtils;
 import io.github.hello09x.fakeplayer.core.manager.FakeplayerList;
 import io.github.hello09x.fakeplayer.core.manager.FakeplayerManager;
 import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.InventoryView;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import static net.kyori.adventure.text.Component.text;
-import static net.kyori.adventure.text.Component.translatable;
 
 /**
- * 默认实现, 无法看到装备栏
+ * Built-in inventory editor with storage, armour and off-hand slots.
  *
  * @author tanyaofei
  * @since 2024/8/12
@@ -29,14 +33,55 @@ public class SimpleInvseeManagerImpl extends AbstractInvseeManager {
 
     @Override
     protected @Nullable InventoryView openInventory(@NotNull Player viewer, @NotNull Player whom) {
-        var view = viewer.openInventory(whom.getInventory());
-        if (view!= null) {
-            view.setTitle(ComponentUtils.toString(translatable(
-                    "fakeplayer.manager.inventory.title",
-                    text(whom.getName())
-            ), viewer.locale()));
+        var holder = new BotInventoryHolder(whom);
+        var inventory = Bukkit.createInventory(holder, 54, text("Bot Inventory: " + whom.getName()));
+        holder.inventory = inventory;
+
+        var storage = whom.getInventory().getStorageContents();
+        for (int slot = 0; slot < Math.min(36, storage.length); slot++) {
+            inventory.setItem(slot, storage[slot]);
         }
-        return view;
+        inventory.setItem(45, whom.getInventory().getHelmet());
+        inventory.setItem(46, whom.getInventory().getChestplate());
+        inventory.setItem(47, whom.getInventory().getLeggings());
+        inventory.setItem(48, whom.getInventory().getBoots());
+        inventory.setItem(50, whom.getInventory().getItemInOffHand());
+        return viewer.openInventory(inventory);
+    }
+
+    @EventHandler
+    public void saveEquipment(@NotNull InventoryCloseEvent event) {
+        if (!(event.getInventory().getHolder() instanceof BotInventoryHolder holder)) {
+            return;
+        }
+        var target = holder.target;
+        if (!target.isOnline() || manager.isNotFake(target)) {
+            return;
+        }
+        var storage = new org.bukkit.inventory.ItemStack[36];
+        for (int slot = 0; slot < storage.length; slot++) {
+            storage[slot] = event.getInventory().getItem(slot);
+        }
+        target.getInventory().setStorageContents(storage);
+        target.getInventory().setHelmet(event.getInventory().getItem(45));
+        target.getInventory().setChestplate(event.getInventory().getItem(46));
+        target.getInventory().setLeggings(event.getInventory().getItem(47));
+        target.getInventory().setBoots(event.getInventory().getItem(48));
+        target.getInventory().setItemInOffHand(event.getInventory().getItem(50));
+    }
+
+    private static final class BotInventoryHolder implements InventoryHolder {
+        private final Player target;
+        private Inventory inventory;
+
+        private BotInventoryHolder(Player target) {
+            this.target = target;
+        }
+
+        @Override
+        public @NotNull Inventory getInventory() {
+            return inventory;
+        }
     }
 
 
